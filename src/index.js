68fca88e88b07be89d5f60863c1c7e90fb594d34
rifathasan71 <refat.hasan.4660@gmail.com>
2025-04-11 20:15:38 +0600

    nestjs first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// src/index.ts
+function greet(name) {
+    return `Hello, ${name}! Welcome to TypeScript.`;
+}
+const userName = "AIUB Student";
+console.log(greet(userName));
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// src/index.ts
+
+function greet(name: string): string {
+    return `Hello, ${name}! Welcome to TypeScript.`;
+  }
+  
+  const userName = "AIUB Student";
+  console.log(greet(userName));
+  
